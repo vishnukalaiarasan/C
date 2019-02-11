a0d08c5aba0e5520d8fb548913119ca87f39bfa1
VishnuKalaiarasan <47056941+vishnukalaiarasan@users.noreply.github.com>
2019-02-11 22:27:16 +0530

    Create Set-1-3.c

---
+++ Set-1-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   char ch;
+   printf("Enter a Character : ");
+   scanf("%c",&ch);
+   if(ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U' )
+   printf("%c is a Vowel. \n",ch);
+   else
+   printf("%c is not a Vowel. \n",ch);
+   return 0;
+}
