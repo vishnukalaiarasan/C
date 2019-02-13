1fb5ea7e5cbcc77cf719abbd5da653d112b3019e
VishnuKalaiarasan <47056941+vishnukalaiarasan@users.noreply.github.com>
2019-02-13 15:38:23 +0530

    Create Set-1-5.c

---
+++ Set-1-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the values : ");
+    scanf("%d%d%d",&a,&b,&c);
+    if( a>=b && a>=c )
+    printf("%d is the largest number.", a);
+    if( b>=a && b>=c )
+    printf("%d is the largest number.", b);
+    if( c>=a && c>=b )
+    printf("%d is the largest number.", c);
+}
