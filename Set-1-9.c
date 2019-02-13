04d85f524c2b1fb22e63ab8345104536b7357714
VishnuKalaiarasan <47056941+vishnukalaiarasan@users.noreply.github.com>
2019-02-13 21:17:30 +0530

    Create Set-1-9.c

---
+++ Set-1-9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i,j,sum=0;
+printf("enter the values:");
+scanf("%d%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",n);
+}
+for(j=1;j<=k;j++)
+{
+sum=sum+j;
+}
+printf("sum is %d",sum);
+return 0;
+}
