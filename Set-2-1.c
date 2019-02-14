48287b650d12d34f7e3eb2beb7acb901aac53c7e
VishnuKalaiarasan <47056941+vishnukalaiarasan@users.noreply.github.com>
2019-02-14 15:52:43 +0530

    Create Set-2-1.c

---
+++ Set-2-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,pow,i,sum=1;
+    scanf("%d%d",&num,&pow);
+    for(i=0;i<pow;i++)
+    {
+        sum=sum*num;
+    }
+    printf("Sum : %d",sum);
+    return 0;
+}
